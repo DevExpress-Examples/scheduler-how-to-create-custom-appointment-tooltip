1b856566d59acfbf545f6db5539716d9b7a5a3b2
artemiy.dmitrochenko <artemiy.dmitrochenko@devexpress.com>
2022-09-13 12:34:47 +0400

    Added Vue example

---
+++ Vue/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <DxList
+        width='33%'
+        :data-source="tooltipItems"
+        item-template="AppointmentInfo"
+    >
+      <template #AppointmentInfo="{data: tooltipItems}">
+        <AppointmentInfo :data="tooltipItems"/>
+      </template>
+    </DxList>
+    <DxScheduler
+        current-view="week"
+        width='66%'
+        :data-source="dataSource"
+        :current-date="currentDate"
+        :resources="resources"
+        :height="600"
+        :start-day-hour="15"
+        :end-day-hour="20"
+        :on-appointment-tooltip-showing="appointmentTooltipShowingHandler"
+    >
+    </DxScheduler>
+  </div>
+</template>
+
+<script>
+import "devextreme/dist/css/dx.common.css";
+import "devextreme/dist/css/dx.material.blue.light.compact.css";
+import {DxScheduler} from "devextreme-vue/scheduler";
+import {DxList} from 'devextreme-vue/list';
+import {data, resources} from "./data.js";
+import Utils from "./utils.js";
+import './style.css';
+import AppointmentInfo from '@/components/AppointmentInfo';
+
+export default {
+  name: "App",
+  components: {
+    AppointmentInfo,
+    DxScheduler,
+    DxList,
+  },
+  data() {
+    return {
+      currentDate: new Date(2021, 5, 2),
+      dataSource: data,
+      resources: resources,
+      tooltipItems: [],
+    };
+  },
+  methods: {
+    appointmentTooltipShowingHandler: function (e) {
+      e.cancel = true;
+      const {appointments} = e;
+      const colors = {};
+      const res = appointments.map((it, idx) => {
+        it.color.done((color) => colors[idx] = color);
+        return {
+          id: idx,
+          text: it.appointmentData.text,
+          startDate: Utils.formatDate(it.appointmentData.startDate),
+          endDate: Utils.formatDate(it.appointmentData.endDate),
+        }
+      });
+      res.forEach((it) => it.color = colors[it.id]);
+      this.tooltipItems = res;
+    },
+  }
+};
+</script>
---
+++ Vue/src/components/AppointmentInfo.vue
@@ -0,0 +1,29 @@
+<template #AppointmentInfo="{ data }">
+  <div class='tooltip'>
+    <div class='marker' :style="getMarkerStyle(data.color)"/>
+    <div>
+      <p>{{ data.text }}</p>
+      <p>{{ `${data.startDate} - ${data.endDate}` }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: () => {},
+    }
+  },
+  methods: {
+    getMarkerStyle(color) {
+      return `background: ${color}`;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
